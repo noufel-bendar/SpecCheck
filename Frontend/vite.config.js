1cb48b34eb323f569f4199f52271e87e6d3007b8
noufel-bendar <bendarnoufel@gmail.com>
2025-08-12 19:15:53 +0100

    some changing in frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< Current (Your changes)
-=======
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
   base: '/',
->>>>>>> Incoming (Background Agent changes)
 })
